50e724113462509b179259329a3a5b6b58413b27
JeonMinJae <ssi02014@naver.com>
2021-05-05 01:00:28 +0900

    refactor(minjae): 리팩토링

--- js/game.js
+++ js/game.js
@@ -13,13 +13,11 @@ let acceptingAnswers = true;
 let score = 0;
 let questionCounter = 0;
 let availableQuestions = [];
-
 let questions = [];
 
 //fetch를 이용해 json 파일 받아오기
 fetch("question.json")
   .then((res) => {
-    console.log(res);
     return res.json(); //json파일을 읽고 JSON 객체 형태로 파싱
   })
   .then((loadedQuestion) => {
@@ -40,6 +38,8 @@ startGame = () => {
 
 //새로운 문제 출력
 getNewQuestion = () => {
+  const questionIndex = Math.floor(Math.random() * availableQuestions.length);
+
   if (availableQuestions.length === 0 || questionCounter >= MAX_QUESTIONS) {
     localStorage.setItem("mostRecentScore", score);
     return window.location.assign("./end.html");
@@ -47,11 +47,10 @@ getNewQuestion = () => {
 
   questionCounter++;
 
+  //Update Progress
   progressText.innerText = `Question ${questionCounter} / ${MAX_QUESTIONS}`;
-
   progressBarFull.style.width = `${(questionCounter / MAX_QUESTIONS) * 100}%`;
 
-  const questionIndex = Math.floor(Math.random() * availableQuestions.length);
   currentQuestion = availableQuestions[questionIndex];
   question.innerHTML = currentQuestion.question;
 
@@ -67,19 +66,17 @@ getNewQuestion = () => {
 
 choices.forEach((choice) => {
   choice.addEventListener("click", (e) => {
-    if (!acceptingAnswers) return;
-
-    acceptingAnswers = false;
-
     const selectedChoice = e.target;
     const selectedAnswer = selectedChoice.dataset["number"]; //data-number="1"에서 1을 의미
 
     let classToApply =
       selectedAnswer == currentQuestion.answer ? "correct" : "incorrect";
 
-    if (classToApply === "correct") {
-      incrementScore(CORRECT_BOUNS);
-    }
+    if (!acceptingAnswers) return;
+
+    acceptingAnswers = false;
+
+    if (classToApply === "correct") incrementScore(CORRECT_BOUNS);
 
     selectedChoice.parentElement.classList.add(classToApply);
 
